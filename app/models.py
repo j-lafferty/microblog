6d9b32da10386fa866eff1313d1389087fc61ad1
James Lafferty <james@j-lafferty.net>
2020-10-20 11:44:35 -0700

    Fix typo in __init__ login variable and circular import errors with flask_login

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
-from datetime import datetime
-from app import db, login
+from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from app import db
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
