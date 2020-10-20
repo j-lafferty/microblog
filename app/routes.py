bd4b0581c6bd96faca3a89f8a9284d22d8ad1d49
James Lafferty <james@j-lafferty.net>
2020-10-20 15:29:34 -0700

    Fix typo in flask import request

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, requests
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
