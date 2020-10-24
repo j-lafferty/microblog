4a8258a3afc07dc2b53352d8cb376a941a808145
James Lafferty <james@j-lafferty.net>
2020-10-23 17:02:42 -0700

    Add followers association table to db

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from flask_login import UserMixin
 
+followers = db.Table('followers',
+	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+	db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index=True, unique=True)
@@ -26,6 +31,12 @@ class User(UserMixin, db.Model):
 		digest= md5(self.email.lower().encode('utf-8')).hexdigest()
 		return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
+	followed = db.relationship(
+		'User', secondary=followers,
+		primaryjoin=(followers.c.follower_id == id),
+		secondaryjoin=(followers.c.followed_id == id),
+		backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	body = db.Column(db.String(140))
@@ -38,4 +49,3 @@ class Post(db.Model):
 @login.user_loader
 def load_user(id):
 	return User.query.get(int(id))
-
