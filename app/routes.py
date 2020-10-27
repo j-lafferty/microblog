ca9cc3d3934765fb3dc152b4eb8d35291d929ce3
James Lafferty <james@j-lafferty.net>
2020-10-27 08:20:57 -0700

    Fix syntax error

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-Mfrom flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user, logout_user, login_required
