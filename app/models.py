0d0cd55aeeb576829f1c71b8d9cb78fe8cecf954
James Lafferty <james@j-lafferty.net>
2020-10-27 10:37:55 -0700

    Move '@login.user_loader' above Post class for organization

--- app/models.py
+++ app/models.py
@@ -56,6 +56,10 @@ class User(UserMixin, db.Model):
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))        
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -65,6 +69,3 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
