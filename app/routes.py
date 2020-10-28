b56082bb997449265e35825e8009781d08466a58
James Lafferty <james@j-lafferty.net>
2020-10-28 07:27:11 -0700

    Add pagination to view functions 'index' and 'explore'

--- app/routes.py
+++ app/routes.py
@@ -12,8 +12,8 @@ def before_request():
 		current_user.last_seen = datetime.utcnow()
 		db.session.commit()
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
 	form = PostForm()
@@ -23,8 +23,10 @@ def index():
 		db.session.commit()
 		flash('Your post is now live!')
 		return redirect(url_for('index'))
-	posts = current_user.followed_posts().all()
-	return render_template('index.html', title='Home', form=form, posts=posts)
+	page = requests.args.get('page', 1, type=int)
+	posts = current_user.followed_posts().paginate(
+		page, app.config['POSTS_PER_PAGE'], False)
+	return render_template('index.html', title='Home', form=form, posts=posts.items)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -129,7 +131,9 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
-	posts = Post.query.order_by(Post.timestamp.desc().all())
-	return render_template('index.html', title='Explore', posts=posts)
+	page = request.args.get('page', 1, type=int)
+	posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+		page, app.config['POSTS_PER_PAGE'], False)
+	return render_template('index.html', title='Explore', posts=posts.items)
 
-	
+
