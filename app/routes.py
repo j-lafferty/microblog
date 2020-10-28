1a9d7310c271e8cc07389e74fd670ed017960cad
James Lafferty <james@j-lafferty.net>
2020-10-28 07:42:15 -0700

    Fix syntax error in 'index' view function's 'page' variable assignment

--- app/routes.py
+++ app/routes.py
@@ -23,7 +23,7 @@ def index():
 		db.session.commit()
 		flash('Your post is now live!')
 		return redirect(url_for('index'))
-	page = requests.args.get('page', 1, type=int)
+	page = request.args.get('page', 1, type=int)
 	posts = current_user.followed_posts().paginate(
 		page, app.config['POSTS_PER_PAGE'], False)
 	next_url = url_for('index', page=posts.next_num) \
