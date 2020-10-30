d6fe266378e88e8a7fe41b36224e32752b922336
James Lafferty <james@j-lafferty.net>
2020-10-30 08:07:49 -0700

    Add reset password token methods

--- app/models.py
+++ app/models.py
@@ -56,6 +56,20 @@ class User(UserMixin, db.Model):
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
+    def get_rest_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECURITY_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECURITY_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))        
