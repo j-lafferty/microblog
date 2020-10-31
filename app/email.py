39ab7d6b3730011d62fabdfe1df856aa0d41ef4c
James Lafferty <james@j-lafferty.net>
2020-10-30 17:09:00 -0700

    Fix syntax error in 'from app import mail'

--- app/email.py
+++ app/email.py
@@ -1,7 +1,7 @@
 from threading import Thread
 from flask import render_template
 from flask_mail import Message
-from app import app, flask_mail
+from app import app, mail
 
 def send_async_email(app, msg):
   with app.app_context():
