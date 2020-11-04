7125b303459a78c3a4960b4265a1674906c2c164
James Lafferty <james@j-lafferty.net>
2020-11-04 08:21:03 -0800

    Add flask-babel instance

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
 
 # initialize app instance
 app = Flask(__name__)
@@ -20,6 +21,7 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 # need to import after app instance has initialized
 from app import routes, models, errors
