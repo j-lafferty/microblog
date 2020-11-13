8b7f82a54f460987cee14aceee95b2ef535cf255
James Lafferty <james@j-lafferty.net>
2020-11-12 17:11:03 -0800

    Fix syntax error

--- app/cli.py
+++ app/cli.py
@@ -11,24 +11,24 @@ def translate():
 def update():
     """Update all languages."""
     if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
-        raise RunTimeError('extract command failed')
+        raise RuntimeError('extract command failed')
     if os.system('pybable update -i messages.pot -d app/translations'):
-        raise RunTimeError('update command failed')
+        raise RuntimeError('update command failed')
     os.remove('messages.pot')
 
 @translate.command()
 def compile():
     """Compile all languages."""
     if os.system('pybable compile -d app/translations'):
-        raise RunTimeError('compile command failed')
+        raise RuntimeError('compile command failed')
 
 @translate.command()
 @click.argument('lang')
 def init(lang):
     """Initialize a new language."""
     if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
-        raise RunTimeError('extract commant failed')
+        raise RuntimeError('extract commant failed')
     if os.system('pybabel init -i messages.pot -d app/translations -l ' + lang):
-        raise RunTimeError('init command failed')
+        raise RuntimeError('init command failed')
     os.remove('messages.pot')
 
