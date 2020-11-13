8f6f19e64cd8616c8cef222bb470fb5a7d239364
James Lafferty <james@j-lafferty.net>
2020-11-13 13:14:37 -0800

    Update for Microsoft Translate v3

--- app/translate.py
+++ app/translate.py
@@ -1,5 +1,7 @@
 import json
 import requests
+import uuid
+import os
 from flask_babel import _
 from app import app
 
@@ -7,11 +9,20 @@ def translate(text, source_language, dest_language):
     if 'MS_TRANSLATOR_KEY' not in app.config or \
             not app.config['MS_TRANSLATOR_KEY']:
         return _('Error: the translation service is not configured.')
-    auth = {'Ocp-Apim-Subscription-Key': app.config['MS_TRANSLATOR_KEY']}
-    r = requests.get('https://api.microsofttranslator.com/v2/Ajax.svc'
-                     '/Translate?text={}&from={}&to={}'.format(
-                        text, source_language, dest_language),
-                     headers=auth)
+    subscription_key = app.config['MS_TRANSLATOR_KEY']
+    endpoint = 'https://api.cognitive.microsofttranslator.com'
+    path = '/translate?api-version=3.0'
+    param = f'&from={source_language}&to={dest_language}'
+    constructed_url = endpoint + path + param
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscription_key,
+        'Ocp-Apim-Subscription-Region': 'westus2',
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+        }
+    body = [{'text': text}]
+    r = requests.post(constructed_url, headers=headers, json=body)
     if r.status_code != 200:
         return _('Error: the translation service failed.')
-    return json.loads(r.content.decode('utf-8-sig'))
+    response = r.json()
+    return response[0]['translations'][0]['text']
