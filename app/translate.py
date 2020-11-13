bcbe391e4cb8bc0ff3749f046b97595c00779fe2
James Lafferty <james@j-lafferty.net>
2020-11-13 11:49:48 -0800

    Fix syntax error

--- app/translate.py
+++ app/translate.py
@@ -4,10 +4,10 @@ from flask_babel import _
 from app import app
 
 def translate(text, source_language, dest_language):
-    if 'MS_TRANSLTOR_KEY' not in app.config or \
-            not app.config['MS_TRANSLTOR_KEY']:
+    if 'MS_TRANSLATOR_KEY' not in app.config or \
+            not app.config['MS_TRANSLATOR_KEY']:
         return _('Error: the translation service is not configured.')
-    auth = {'Ocp-Apim-Subscription-Key': app.config['MS_TRANSLTOR_KEY']}
+    auth = {'Ocp-Apim-Subscription-Key': app.config['MS_TRANSLATOR_KEY']}
     r = requests.get('https://api.microsofttranslator.com/v2/Ajax.svc'
                      '/Translate?text={}&from={}&to={}'.format(
                         text, source_language, dest_language),
